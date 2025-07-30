781556c52003ef496cf17d922e934d40206ef2ca
grosjeanma <grosjean.marcel@gmail.com>
2025-07-30 15:28:30 +0200

    better build for macos

--- build_macos.sh
+++ build_macos.sh
@@ -1,22 +1,19 @@
 
-# install python dependencies
-pip3 install -r src-python/requirements.txt --break-system-packages
-pip3 install pyinstaller --break-system-packages
-
-# build python executable
-rm -rf src-python/build src-python/dist
-pyinstaller --onedir src-python/main.py --distpath src-python/dist --workpath src-python/build
-
+rm -rf dist
+rm -rf src-python/build
 
 # install node dependencies
 npm install
 
 # build electron app
-rm -rf dist/electron
 quasar build -m electron
 
-# copy python executable to electron app
-cp -r src-python/dist/main dist/electron/UnPackaged/main
+# install python dependencies
+pip3 install -r src-python/requirements.txt --break-system-packages
+pip3 install pyinstaller --break-system-packages
+
+# build python executable
+pyinstaller --onedir src-python/main.py --distpath dist/electron/UnPackaged --workpath src-python/build
 
 # repackage electron app
 npx electron-packager dist/electron/UnPackaged Samantha --platform=darwin --out=dist/electron/Packaged --overwrite  --icon=src-electron/icons/icon.png
