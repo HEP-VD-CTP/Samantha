a3ff7834387422592bb8130b9fd12739b7c3e55e
grosjeanma <grosjean.marcel@gmail.com>
2025-08-10 16:42:15 +0200

    macos installer

--- build_macos.sh
+++ build_macos.sh
@@ -18,3 +18,16 @@ pyinstaller --onedir src-python/main.py --distpath dist/electron/UnPackaged --wo
 # repackage electron app
 npx electron-packager dist/electron/UnPackaged Samantha --platform=darwin --out=dist/electron/Packaged --overwrite  --icon=src-electron/icons/icon.png
 
+# remove unnecessary files
+rm -rf dist/electron/Packaged/Samantha-darwin-arm64/LICENSE
+rm -rf dist/electron/Packaged/Samantha-darwin-arm64/LICENSES.chromium.html
+rm -rf dist/electron/Packaged/Samantha-darwin-arm64/version
+
+# remove the quarantine attribute
+xattr -cr dist/electron/Packaged/Samantha-darwin-arm64/Samantha.app
+
+# create a .pkg installer
+pkgbuild --root dist/electron/Packaged/Samantha-darwin-arm64 \
+         --identifier ch.hepl.samantha \
+         --version 1.0.0 \
+         --install-location /Applications dist/electron/Packaged/Samantha-macos.pkg
